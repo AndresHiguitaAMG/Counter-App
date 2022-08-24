b6b9d641d53247986b40ebaff3603cdb97a9cfd2
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-08-24 11:46:16 -0500

    new change

--- src/tests/base/08-imp-exp.test.js
+++ src/tests/base/08-imp-exp.test.js
@@ -2,7 +2,7 @@ import { getHeroeById, getHeroesByOwner } from "../../base/08-imp-exp";
 import heroes from "../../data/heroes";
 
 describe('Pruebas en funciones de Heroes', () => {
-    test('deber retornar un heroe por id', () => {
+    test('debe retornar un heroe por id', () => {
         const id = 1;
         const heroe = getHeroeById(id);
         const heroeData = heroes.find(h => h.id === id);
@@ -10,14 +10,14 @@ describe('Pruebas en funciones de Heroes', () => {
         expect(heroe).toEqual(heroeData);
     });
 
-    test('deber retornar undefined si el heroe no existe', () => {
+    test('debe retornar undefined si el heroe no existe', () => {
         const id = 10;
         const heroe = getHeroeById(id);
 
         expect(heroe).toBe(undefined);
     });
 
-    test('deber retornar un arreglo con los heroes de DC', () => {
+    test('debe retornar un arreglo con los heroes de DC', () => {
         const owner = "DC";
         const heroeOwner = getHeroesByOwner(owner);
         const heroeDataOwner = heroes.filter(o => o.owner === owner);
@@ -26,7 +26,7 @@ describe('Pruebas en funciones de Heroes', () => {
         expect(heroeOwner).toEqual(heroeDataOwner);
     });
 
-    test('deber retornar un arreglo con los heroes de Marvel', () => {
+    test('debe retornar un arreglo con los heroes de Marvel', () => {
         const ownerMarvel = "Marvel";
         const heroeOwnerM = getHeroesByOwner(ownerMarvel);
         console.log(heroeOwnerM);
