ed0acae3337163431918c1868e60d39bfa023644
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-09-05 23:55:56 -0500

    new test 09-promesas

--- src/base-pruebas/09-promesas.js
+++ src/base-pruebas/09-promesas.js
@@ -6,13 +6,11 @@ export const getHeroeByIdAsync = ( id ) => {
     return new Promise( (resolve, reject) => {
 
         setTimeout( () =>  {
-            // Tarea
-            // importen el 
             const p1 = getHeroeById( id );
             if ( p1 ) {
                 resolve( p1 );
             } else {
-                reject(`No se pudo encontrar el héroe con id ${id}`);
+                reject("No se pudo encontrar el héroe con id " + id);
             }
         }, 1000)
     
