8b602493b1f20367c5c22b13e968349e364414a1
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-09-05 23:18:35 -0500

    new test 08-imp-exp

--- tests/base-pruebas/08-imp-exp.test.js
+++ tests/base-pruebas/08-imp-exp.test.js
@@ -19,12 +19,49 @@ describe('Pruebas en 08-imp-exp', () => {
 
         expect(hero).toBeFalsy();
     });
-
+    
+    //Mi solución:
     test('getHeroesByOwner debe retornar los heroes de DC', () => {
         const ownerHero = "DC";
         const ownerTest = getHeroesByOwner(ownerHero);
         const findHeroByOwner = heroes.filter((h) => h.owner === ownerHero);
 
-        expect(ownerTest).toStrictEqual(findHeroByOwner);
+        expect(ownerTest).toEqual(findHeroByOwner);
     });
+
+    test('getHeroesByOwner debe retornar los heroes de Marvel', () => {
+        const ownerHeroM = "Marvel";
+        const ownerTestM = getHeroesByOwner(ownerHeroM);
+        const findHeroByOwnerM = heroes.filter((he) => he.owner === ownerHeroM);
+
+        expect(ownerTestM).toEqual(findHeroByOwnerM);
+    });
+    
+    //Solución del instructor:
+    // test('getHeroesByOwner debe de regresar heroes de DC', () => {
+        
+    //     const owner = 'DC';
+    //     const heroes = getHeroesByOwner( owner );
+
+    //     expect( heroes.length ).toBe( 3 );
+    //     expect( heroes ).toEqual([
+    //         { id: 1, name: 'Batman', owner: 'DC' },
+    //         { id: 3, name: 'Superman', owner: 'DC' },
+    //         { id: 4, name: 'Flash', owner: 'DC' }
+    //     ]);
+    //     expect( heroes ).toEqual( heroes.filter( (heroe) => heroe.owner === owner ) )
+
+
+    // });
+
+    // test('getHeroesByOwner debe de regresar heroes de Marvel', () => {
+        
+    //     const owner = 'Marvel';
+    //     const heroes = getHeroesByOwner( owner );
+
+    //     expect( heroes.length ).toBe( 2 );
+    //     expect( heroes ).toEqual( heroes.filter( (heroe) => heroe.owner === owner ) )
+        
+
+    // });
 });
